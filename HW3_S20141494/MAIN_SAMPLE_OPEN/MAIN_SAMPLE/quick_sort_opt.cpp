b5fa31d7a5299410ffcaba851328f98ca896771e
kdw9502 <kdw9502@gmail.com>
2017-11-26 20:57:20 +0900

    datas

--- HW3_S20141494/MAIN_SAMPLE_OPEN/MAIN_SAMPLE/quick_sort_opt.cpp
+++ HW3_S20141494/MAIN_SAMPLE_OPEN/MAIN_SAMPLE/quick_sort_opt.cpp
@@ -1,9 +1,35 @@
 #include <stdio.h>
 
 #include "my_types.h"
+#define ELEMENT_SWAP(a,b) {ELEMENT temp;temp=a;a=b;b=temp;}
+#define MIN(a,b) (((a)<(b))?(a):(b))
+inline int pivot_select(ELEMENT*, int, int);
 
+int pivot_select(ELEMENT *data, int left, int right) {
+	return MIN(data[left].key, data[(left + right) / 2].key) ^ MIN(data[(left + right) / 2].key, data[right].key) ^ MIN(data[left].key, data[right].key);
+}
 int QUICK_SORT_OPT(ELEMENT data[], int left, int right) {
 
+	int i = left, j = right;
+	ELEMENT temp;
+	int pivot = pivot_select(data, left, right);
 
+	while (i <= j) {
+		while (data[j].key > pivot)
+			j--;
+		while (data[i].key < pivot)
+			i++;
+		if (i <= j) {
+			ELEMENT_SWAP(data[i], data[j]);
+			i++;
+			j--;
+		}
+	};
+
+	if (left < j)
+		QUICK_SORT(data, left, j);
+	if (i < right)
+		QUICK_SORT(data, i, right);
 	return 1;
+
 }
