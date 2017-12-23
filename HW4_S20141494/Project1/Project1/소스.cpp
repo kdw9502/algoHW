db5ce313c41372f849fdb607fadca9e0397aebdf
kdw9502 <kdw9502@gmail.com>
2017-12-24 03:52:41 +0900

    complete

--- HW4_S20141494/Project1/Project1/소스.cpp
+++ HW4_S20141494/Project1/Project1/소스.cpp
@@ -2,11 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <direct.h>
+#include <algorithm>
+#include <vector>
+
+#include <functional>
+using namespace std;
 #define MAX(a,b) {(a>b)?a:b}
 #define MIN(a,b) {(a>b)?b:a}
 #define P1
 #define P2
 #define P3
+#ifdef P3
+struct CLASS {
+	int s;
+	int f;
+};
+
+#endif 
 
 #ifdef P1
 void problem1(int len, char* string, FILE* fp_w){
@@ -36,12 +48,14 @@ void problem1(int len, char* string, FILE* fp_w){
 	for(i=0;i<len;i++)
 		for(j=0;j<len;j++)mat[j][i]=mat[i][j];
 	fprintf(fp_w,"%d\n",len_lps=mat[0][len-1]);
+	int max_len = len_lps;
 	char *lps = new char[len_lps + 1];
 	lps[len_lps] = 0;
-	for (i = 0,j=len-1; i<len&&j>0;) {
+	for (i = 0,j=len-1; i<len&&j>0 &&i<=j;) {
 		if (string[i] == string[j])
 		{
 			lps[len_lps - 1] = string[i];
+			lps[max_len - len_lps] = string[i];
 			i++;
 			j--;
 			len_lps--;
@@ -110,6 +124,48 @@ void problem2(int len, char* string, FILE* fp_w) {
 }
 #endif
 
+#ifdef P3
+int start_compare(const void *a, const void *b) {
+	struct CLASS *cla = (struct CLASS*)a;
+	struct CLASS *clb = (struct CLASS*)b;
+	if (cla->s > clb->s) return 1;
+	else if (cla->s == clb->s) return 0;
+	else return -1;
+}
+
+void problem3(int len,struct CLASS *cl, FILE* fp_w) {
+
+	qsort(cl,len,sizeof(struct CLASS),start_compare);
+	int d = 1;
+	int r[1024],ck;
+	r[0]=cl[0].f;
+
+	vector<int> Q;
+	Q.reserve(1024);
+	Q.push_back(r[0]);
+	push_heap(Q.begin(), Q.end(), greater<>());
+	for (int j = 1; j < len; j++)
+	{
+		ck = Q.front();
+
+		if (cl[j].s >= ck) {
+			ck=cl[j].f;
+			pop_heap(Q.begin(), Q.end(), greater<>());
+			Q.pop_back();
+			Q.push_back(ck);
+			push_heap(Q.begin(), Q.end(), greater<>());
+		}
+		else {
+			d++;
+			r[d] = cl[j].f;
+			Q.push_back(r[d]);
+			push_heap(Q.begin(), Q.end(), greater<>());
+		}
+
+	}
+	fprintf(fp_w,"%d\n",d);
+}
+#endif
 
 int main() {
 	_mkdir("20141494_OUTPUT");
@@ -146,6 +202,24 @@ int main() {
 		char *string = new char[len];
 		fscanf(fp_r, "%s", string);
 		problem2(len, string, fp_w);
+	}
+	fclose(fp_r); fclose(fp_w);
+#endif
+
+#ifdef P3
+	fp_r = fopen("input-3-3.txt", "r");
+	fp_w = fopen("20141494_OUTPUT/output-3-3.txt", "w");
+	fscanf(fp_r, "%d", &N);
+	fprintf(fp_w, "%d\n", N);
+	struct CLASS *cl;
+	for (int i = 0; i < N; i++)
+	{
+		int len;
+		fscanf(fp_r, "%d", &len);
+		cl = (struct CLASS*)malloc(sizeof(struct CLASS)*len);
+		for (int i = 0; i < len; i++)
+			fscanf(fp_r, "%d %d", &cl[i].s,&cl[i].f);
+		problem3(len, cl, fp_w);
 	}
 	fclose(fp_r); fclose(fp_w);
 #endif
